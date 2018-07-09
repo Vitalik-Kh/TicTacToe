008c979e12c628f5052b86e746cea87346708595
Vitalii Khymynets <vtlkand@gmail.com>
2018-07-09 12:18:31 +0100

    Delete ifElse AI from master

--- src/_AI/AImethods/centerSquareMove.js
+++
@@ -1,9 +0,0 @@
-const centerSquareMove = (data) => {
-    let nextMove = null;
-    if (data.field[4] === null) {
-        nextMove = 4;
-    }
-    return nextMove;
-}
-
-export default centerSquareMove;
--- src/_AI/utility/validateMoves.js
+++
@@ -1,8 +0,0 @@
-const validateMoves = (data, moves) => {
-    const validMoves = moves.filter(move => {
-        return data.field[move] == null ? true : false;
-    });
-    return validMoves.length > 0 ? validMoves : null;
-}
-
-export default validateMoves;
